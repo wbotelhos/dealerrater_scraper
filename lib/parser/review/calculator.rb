876f87f0f132117b439d7dc91d2a646e2781311d
Washington Botelho <wbotelhos@gmail.com>
2021-01-09 17:46:01 -0300

    ref: renames json var to review

--- lib/parser/review/calculator.rb
+++ lib/parser/review/calculator.rb
@@ -5,27 +5,27 @@ module Parser
     module Calculator
       module_function
 
-      def call(json)
-        json.map do |item|
-          item.tap { item[:ratings] = rating_by_area(item) }
+      def call(reviews)
+        reviews.map do |review|
+          review.tap { review[:ratings] = rating_by_area(review) }
         end
       end
 
-      def rating_by_area(json)
+      def rating_by_area(review)
         {
-          content:           rating_for_content(json),
-          dealership_rating: rating_for_dealership_rating(json),
-          employees_ratings: rating_for_employees_ratings(json),
-          features_ratings:  rating_for_features_ratings(json),
-          recommend:         rating_for_recommend(json),
+          content:           rating_for_content(review),
+          dealership_rating: rating_for_dealership_rating(review),
+          employees_ratings: rating_for_employees_ratings(review),
+          features_ratings:  rating_for_features_ratings(review),
+          recommend:         rating_for_recommend(review),
         }
       end
 
-      def rating_for_content(json)
+      def rating_for_content(review)
         bad_words  = %w[bad]
         good_words = %w[good nice]
 
-        json[:content].split(' ').inject(0) do |total, word|
+        review[:content].split(' ').inject(0) do |total, word|
           total += 1 if good_words.include?(word)
           total -= 1 if bad_words.include?(word)
 
@@ -33,20 +33,20 @@ module Parser
         end
       end
 
-      def rating_for_dealership_rating(json)
-        json[:dealership_rating]
+      def rating_for_dealership_rating(review)
+        review[:dealership_rating]
       end
 
-      def rating_for_employees_ratings(json)
-        json[:employees_ratings].sum { |item| item[:rating] }
+      def rating_for_employees_ratings(review)
+        review[:employees_ratings].sum { |item| item[:rating] }
       end
 
-      def rating_for_features_ratings(json)
-        json[:features_ratings].values.sum
+      def rating_for_features_ratings(review)
+        review[:features_ratings].values.sum
       end
 
-      def rating_for_recommend(json)
-        json[:recommend] ? 1 : 0
+      def rating_for_recommend(review)
+        review[:recommend] ? 1 : 0
       end
     end
   end
